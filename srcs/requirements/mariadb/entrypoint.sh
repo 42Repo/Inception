7ca6bc26bcb8a6859e7693d061158d6b8b4412c9
asuc <sucaurel@gmail.com>
2024-11-01 17:05:29 +0100

    Enhanced Docker setup for WordPress and MariaDB

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-service mysql start
+mysqld_safe --skip-networking &
+
+until mysqladmin ping --silent; do
+    echo "Waiting for MariaDB to be ready..."
+    sleep 2
+done
 
 mysql -u root << EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
@@ -10,6 +15,6 @@ GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 
 exec mysqld_safe
